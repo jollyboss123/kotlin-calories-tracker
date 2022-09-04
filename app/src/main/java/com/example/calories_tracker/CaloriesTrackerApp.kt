7d5b93646d65709b1636790917855843511d4f1c
Fu Sing <98voonfusing@gmail.com>
2022-09-04 21:20:52 +0800

    + Added shared preferences to persist user info

---
+++ app/src/main/java/com/example/calories_tracker/CaloriesTrackerApp.kt
@@ -0,0 +1,10 @@
+package com.example.calories_tracker
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CaloriesTrackerApp: Application()
+{
+
+}
---
+++ core/src/main/java/com/example/core/domain/model/ActivityLevel.kt
@@ -0,0 +1,21 @@
+package com.example.core.domain.model
+
+sealed class ActivityLevel(val name: String)
+{
+    object Low: ActivityLevel("low")
+    object Medium: ActivityLevel("medium")
+    object High: ActivityLevel("high")
+
+    companion object
+    {
+        fun fromString(name: String): ActivityLevel
+        {
+            return when(name)
+            {
+                "low" -> Low
+                "medium" -> Medium
+                else -> High
+            }
+        }
+    }
+}
